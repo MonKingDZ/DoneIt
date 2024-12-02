4191c7a1b7553a58707787db5393dc610e802316
MonKinDZ <monkingdz@gmail.com>
2024-12-03 00:10:29 +0800

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'DONE IT !',
     );
   }
 }
